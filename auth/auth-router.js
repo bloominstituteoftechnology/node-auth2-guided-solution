c11213385d27c8791bf958dec8977883c3a209c3
Luis Hernandez <luishrd@gmail.com>
2020-05-19 09:07:13 -0600

    updates auth and user code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,58 +1,75 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken'); // installed this library
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); // npm install
 
-const secrets = require('../config/secrets.js');
-const Users = require('../users/users-model.js');
+const router = require("express").Router();
 
-// for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+const secrets = require("../config/secrets.js");
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
+
+router.post("/register", (req, res) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
+  if (isValid(user)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(user.password, rounds);
+
+    user.password = hash;
+
+    Users.add(user)
+      .then(saved => {
+        const token = generateToken(saved);
+
+        res.status(201).json({ data: { user, token } });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
     });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user); // new line
+  if (isValid(req.body)) {
+    Users.findBy({ username })
+      .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user); // new line
 
-        // the server needs to return the token to the client
-        // this doesn't happen automatically like it happens with cookies
-        res.status(200).json({
-          message: `Welcome ${user.username}!, have a token...`,
-          token, // attach the token as part of the response
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
+          // the server needs to return the token to the client
+          // this doesn't happen automatically like it happens with cookies
+          res.status(200).json({
+            message: `Welcome ${user.username}!, have a token...`,
+            token, // attach the token as part of the response
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
     });
+  }
 });
 
 function generateToken(user) {
   const payload = {
     subject: user.id,
     username: user.username,
-    roles: ['Student'],
+    role: user.role,
   };
 
   const options = {
-    expiresIn: '1d',
+    expiresIn: "1d",
   };
 
   return jwt.sign(payload, secrets.jwtSecret, options);
--- users/users-model.js
+++ users/users-model.js
@@ -1,54 +1,39 @@
-const db = require('../database/dbConfig.js');
+const db = require("../database/dbConfig.js");
 
 module.exports = {
   add,
   find,
   findBy,
   findById,
-  addRoles,
 };
 
 function find() {
-  return db('users as u')
-    .join('roles as r', 'u.role_id', '=', 'r.id')
-    .select('u.id', 'u.username', 'r.name as role');
+  return db("users as u")
+    .join("roles as r", "u.role", "=", "r.id")
+    .select("u.id", "u.username", "r.name as role");
 }
 
 function findBy(filter) {
-  return db('users')
-    .join('roles as r', 'u.role_id', '=', 'r.id')
-    .select('u.id', 'u.username', 'r.name as role')
+  return db("users")
+    .join("roles as r", "u.role", "=", "r.id")
+    .select("u.id", "u.username", "r.name as role")
     .where(filter);
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user);
-  const roles = [{ user_id: id, role_id: 3 }];
-  if (user.roles && user.roles.length) {
-    roles = user.roles.map(roleId => {
-      return { user_id: id, role_id: roleId };
-    });
-    await db('user_roles').insert(roles, 'id');
-  }
+  try {
+    const [id] = await db("users").insert(user, "id");
 
-  return findById(id);
+    return findById(id);
+  } catch (error) {
+    throw error;
+  }
 }
 
 function findById(id) {
-  return db('users')
-    .join('roles as r', 'u.role_id', '=', 'r.id')
-    .select('u.id', 'u.username', 'r.name as role')
+  return db("users")
+    .join("roles as r", "u.role", "=", "r.id")
+    .select("u.id", "u.username", "r.name as role")
     .where({ id })
     .first();
-}
-
-function addRoles(userId, roles) {
-  if (userId && Array.isArray(roles) && roles.length) {
-    const roles = roles.map(roleId => {
-      return { user_id: id, role_id: roleId };
-    });
-    db('user_roles').insert(roles, 'id');
-  }
-
-  return findById(userId);
 }
