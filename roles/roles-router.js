0dc3693ec51a6e491527f9d990caf286d4818d22
Luis Hernandez <luishrd@gmail.com>
2020-05-19 09:15:01 -0600

    returns http status code on roles router

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,14 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Roles = require('./roles-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Roles = require("./roles-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', restricted, (req, res) => {
+router.get("/", restricted, (req, res) => {
   Roles.find()
     .then(roles => {
-      res.json(roles);
+      res.status(200).json(roles);
     })
-    .catch(err => res.send(err));
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
 });
 
 module.exports = router;
